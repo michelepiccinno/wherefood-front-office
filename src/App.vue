b7f82de13cf72c81f460f58f2f9cc21db9154cfa
andreafrancioni <andreafrancioni.work@gmail.com>
2024-03-04 15:45:52 +0100

    Deleted last filtered restaurants

--- src/App.vue
+++ src/App.vue
@@ -22,33 +22,16 @@ export default {
 	mounted() {
 		this.doThings();
 
-		// if (this.store.selectedCategories.length == 0) {
-		// 	//Recupera tutti i ristoranti da backend.
-		// 	let restaurantsUrl = this.store.apiUrl + this.store.apiRestaurants;
+		//Recupera tutti i ristoranti da backend.
+		let restaurantsUrl = this.store.apiUrl + this.store.apiRestaurants;
 
-		// 	axios.get(restaurantsUrl).then(risultato => {
-		// 		this.store.restaurantsArray = risultato.data.results;
+		axios.get(restaurantsUrl).then(risultato => {
+			this.store.restaurantsArray = risultato.data.results;
 
-		// 		console.log(risultato);
-		// 	}).catch(errore => {
-		// 		console.error(errore);
-		// 	});
-		// } else {
-		//Recupera i ristoranti filtrati
-
-		this.store.selectedCategories.forEach(selectedCategory => {
-			let restaurantsUrl = this.store.apiUrl + this.store.apiRestaurants + this.store.apiCategories + selectedCategory;
-			axios.get(restaurantsUrl).then(risultato => {
-				this.store.restaurantsArray += risultato.data.results;
-
-				console.log(risultato);
-			}).catch(errore => {
-				console.error(errore);
-			});
-		})
-
-
-		// }
+			console.log(risultato);
+		}).catch(errore => {
+			console.error(errore);
+		});
 
 		//Recupera tutte le categorie da backend.
 		let categoriesUrl = this.store.apiUrl + this.store.apiCategories;
@@ -70,7 +53,7 @@ export default {
 	methods: {
 		doThings() {
 			console.log("App.vue does things");
-		}
+		},
 
 	}
 }

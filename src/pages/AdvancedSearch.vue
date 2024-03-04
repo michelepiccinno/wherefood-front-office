4bef4469ae72bce3435e23e4ccf5584dd91e3834
Emanuele-Guerra <emaguerra1993@hotmail.it>
2024-03-04 12:20:40 +0100

    search styling

--- src/pages/AdvancedSearch.vue
+++ src/pages/AdvancedSearch.vue
@@ -26,15 +26,48 @@ export default {
 }
 </script>
 <template>
-    <div class="wrapper d-flex align-items-center flex-wrap">
-        <AppRestaurantCard v-for="(restaurant, index) in this.store.restaurantsArray" :key="index" :restaurant="restaurant">
-        </AppRestaurantCard>
-    </div>
+    <section>
+        <h2 class="text-center">RISULTATI PER</h2>
+        <div class="d-flex justify-content-around">
+
+            <div class="cat-wrapper">
+                CATEGORIE
+            </div>
+            <div class="wrapper d-flex align-items-center flex-wrap justify-content-between">
+
+                <AppRestaurantCard v-for="(restaurant, index) in this.store.restaurantsArray" :key="index"
+                    :restaurant="restaurant">
+                </AppRestaurantCard>
+            </div>
+        </div>
+
+    </section>
 </template>
 
 <style scoped lang="scss">
+section {
+    background-image: url(search-bg.jpg);
+    background-size: cover;
+    padding: 0 5rem 3rem;
+
+}
+
 .wrapper {
-    width: 80%;
+    height: 100%;
+    width: 60%;
+    border-radius: 10px;
+    overflow-y: auto;
     margin: auto;
+    background-color: rgba(0, 0, 0, 0.800);
+    overflow-y: auto;
+}
+
+
+
+.cat-wrapper {
+    height: 400px;
+    width: 30%;
+    background-color: rgba(0, 0, 0, 0.800);
+    border-radius: 10px;
 }
 </style>

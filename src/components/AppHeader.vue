ed5956eebadaf87fd85de6a56c0d50cba543b343
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2024-03-06 17:25:18 +0100

    Add Cart fuction new

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,55 +7,59 @@ export default {
         return {
             isCartOpen: false,
             store
-        }
+        };
     },
     methods: {
         toggleCart() {
             this.isCartOpen = !this.isCartOpen;
         },
         addToCart(product) {
-            if (this.isInCart(product)) {
-                this.updateCart(product, product.quantity);
+            const cartProduct = this.store.cartItems.find(item => item.id === product.id);
+            if (cartProduct) {
+                // Se il prodotto è già nel carrello, rimuovilo
+                this.removeFromCart(cartProduct.id);
             } else {
+                // Altrimenti, aggiungi il prodotto al carrello
+                product.quantity = 1;
                 this.store.cartItems.push(product);
+                product.isAdded = true; // Imposta il flag isAdded a true per questo prodotto
             }
 
             localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
         },
-        removeFromCart(index) {
-            if (this.store.cartItems[index].quantity <= 1) {
+        removeFromCart(productId) {
+            const index = this.store.cartItems.findIndex(item => item.id === productId);
+            if (index !== -1) {
+                // Rimuovi il prodotto dal carrello
                 this.store.cartItems.splice(index, 1);
-            } else {
-                this.store.cartItems[index].quantity--;
+                // Imposta il flag isAdded a false per questo prodotto
+                const product = this.store.products.find(product => product.id === productId);
+                if (product) {
+                    product.isAdded = false;
+                }
             }
 
-      localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
+            localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
+        },
+        incrementQuantity(index) {
+            this.store.cartItems[index].quantity++;
+            localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
+        },
+        decrementQuantity(index) {
+            if (this.store.cartItems[index].quantity > 1) {
+                this.store.cartItems[index].quantity--;
+                localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
+            }
+        },
     },
-    updateCart(product, quantity) {
-      const index = this.store.cartItems.findIndex(item => item.id === product.id);
-      if (index !== -1) {
-        this.store.cartItems[index].quantity += quantity;
-      }
-    },
-    isInCart(product) {
-      return this.store.cartItems.some(item => item.id === product.id);
-    },
-    incrementQuantity(index) {
-      this.store.cartItems[index].quantity++;
-      localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
-    },
-    decrementQuantity(index) {
-      if (this.store.cartItems[index].quantity > 1) {
-        this.store.cartItems[index].quantity--;
-        localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
-      }
-    },
-  },
-  computed: {
-    totalCartItems() {
-      return this.store.cartItems.reduce((total, item) => total + item.quantity, 0);
+    computed: {
+        totalCartItems() {
+            return this.store.cartItems.reduce((total, item) => total + item.quantity, 0);
+        },
+        totalOrderPrice() {
+            return (index) => (this.store.cartItems[index].price * this.store.cartItems[index].quantity).toFixed(2);
+        }
     }
-  }
 }
 </script>
 
@@ -76,21 +80,48 @@ export default {
         <i class="fa-solid fa-cart-shopping"><span>Carrello</span></i>
         <span class="StyleCount" v-if="store.cartItems.length > 0">({{ totalCartItems }})</span>
       </button>
-      <div class="offcanvas offcanvas-end custom-offcanvas" style="width: 500px;" :class="{ 'show': isCartOpen }" id="cartOffcanvas" tabindex="-1">
+      <div class="offcanvas offcanvas-end custom-offcanvas" style="width: 600px;" :class="{ 'show': isCartOpen }" id="cartOffcanvas" tabindex="-1">
         <div class="offcanvas-header">
-          <h5 class="offcanvas-title">Il tuo carrello</h5>
+          <h5 class="offcanvas-title">Il Tuo Carrello</h5>
           <button @click="toggleCart" type="button" class="btn-close" data-bs-dismiss="offcanvas"
             aria-label="Close">X</button>
         </div>
         <div class="offcanvas-body">
-          <ul v-if="store.cartItems.length > 0">
-            <li class="listyle" v-for="(item, index) in store.cartItems" :key="index">
-              {{ item.name }} - {{ item.price }} € x {{ item.quantity }}
-              <button class="increase-button" @click="incrementQuantity(index)">+</button>
-              <button class="decrease-button" @click="decrementQuantity(index)">-</button>
+          <div class="row">
+            <div class="col-12 d-flex">
+              <div class="ProdottoStyle">
+                  Prodotto
+              </div>
+              <div class="PrezzoStyle">
+                  Prezzo
+              </div>
+              <div class="QuantitaStyle">
+                  Quantità
+              </div>
+            </div>
+          </div>
+          <ul class="" v-if="store.cartItems.length > 0">
+            <li class="d-flex align-items-center" v-for="(item, index) in store.cartItems" :key="index"> 
+              <div class="d-flex align-items-center liststyle">
+                <div class="NameStyle">
+                    {{ item.name }} 
+                </div>
+               <div class="PriceStyle">
+                   {{ totalOrderPrice(index) }}€
+                </div>
+              </div>
+              <div>
+                    <div class="ButtonContainer">
+                       <div class="QuantityStyle ">
+                         {{ item.quantity }}
+                        </div>
+                        <button class="increase-button m-2" @click="incrementQuantity(index)">+</button>
+                        <button class="decrease-button m-2" @click="decrementQuantity(index)">-</button>          
+                    </div>
+              </div>
             </li>
           </ul>
-          <p v-else>Il carrello è vuoto</p>
+          <div class="EmptyCart" v-if="store.cartItems.length === 0">Il carrello è vuoto</div>
         </div>
       </div>
     </div>
@@ -98,7 +129,115 @@ export default {
 </template>
 
 
+
+
+
 <style scoped lang="scss">
+
+.ProdottoStyle
+{
+  margin: 0px 5px 10px 20px;
+  padding: 8px 10px;
+  color: #41b60b;
+  border: 2px solid #333;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px
+}
+.totalpriceStyle{
+   margin: 0px 5px 10px 20px;
+  padding: 8px 10px;
+  color: #41b60b;
+  border: 2px solid #333;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px
+}
+.PrezzoStyle{
+  margin: 0px 20px 10px 22px;
+  padding: 8px 10px;
+  color: #41b60b;
+  border: 2px solid #333;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px
+}
+.QuantitaStyle{
+  margin: 0px 20px 10px 25px;
+  padding: 8px 10px;
+  color: #41b60b;
+  border: 2px solid #333;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px
+}
+.PrezzoTotaleStyle{
+ margin: 0px 10px 10px 25px;
+  padding: 8px 10px;
+  color: #41b60b;
+  border: 2px solid #333;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px
+
+}
+.QuantityStyle{
+
+  color: #41b60b;
+  border: 2px solid #333;
+  padding: 8px 15px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px;
+  margin-left: 17px;
+  margin-right: 35px;
+  
+}
+.EmptyCart{
+  margin-top: 200px;
+  margin-left: 140px;
+  font-weight: bold;
+  font-size: 30px;
+}
+.priceStyle{
+  margin-left: 30px;
+}
+
+.liststyle {
+  width: 250px;
+}
+.NameStyle{
+  width: 120px;
+}
+.ButtonContainer{
+  display: flex;
+    width: 200px;
+  align-items: center;
+}
 .remove-button {
   background-color: transparent;
   color: #333;
@@ -110,9 +249,36 @@ export default {
   text-transform: uppercase;
   cursor: pointer;
   transition: all 0.3s ease;
+  height: 41px;
 }
-
-.remove-button:hover {
+.decrease-button{
+   background-color: transparent;
+  color: #333;
+  border: 2px solid #333;
+  padding: 8px 15px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px;
+}
+.increase-button{
+  background-color: transparent;
+  color: #333;
+  border: 2px solid #333;
+  padding: 8px 15px;
+  border-radius: 5px;
+  font-size: 14px;
+  font-weight: bold;
+  text-transform: uppercase;
+  cursor: pointer;
+  transition: all 0.3s ease;
+  height: 41px;
+}
+.decrease-button:hover,
+.increase-button:hover {
     background-color: #333;
     color: #fff;
 }
@@ -123,15 +289,10 @@ export default {
     padding: 0 5%;
     background-color: #000000;
 
-
-
-
     i {
         color: #575756;
         font-size: 15px;
         padding: 0.5rem;
-
-
     }
 
     i:hover {
@@ -160,7 +321,6 @@ img {
 
 .searchBtn {
     i {
-
         font-size: 20px !important;
     }
 }
@@ -173,10 +333,6 @@ img {
 }
 
 .search {
-
-
-
-
     &__input {
         width: 100%;
         padding: 12px 24px;
@@ -232,13 +388,13 @@ img {
 
 
 .offcanvas-body {
-  background-color: #575756;
-  opacity: 0.5;
+  background-color: #000000;
+
   color: white;
 }
 
 .offcanvas-header {
-  background-color: #575756;
+  background-color: #000000;
   color: white;
 
     .offcanvas-title {

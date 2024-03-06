fb3a9a6053e0052e8955a9085f96aafa5fe28d0c
michelepiccinno <michele.piccinno@gmail.com>
2024-03-06 11:49:34 +0100

    add router-link (to home) to img logo (WHEREFOOD.png)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,32 +2,32 @@
 import { store } from "../store.js";
 
 export default {
-    name: "AppHeader",
-    data() {
-        return {
-            isCartOpen: false,
-            store
-        }
+  name: "AppHeader",
+  data() {
+    return {
+      isCartOpen: false,
+      store
+    }
+  },
+  methods: {
+    toggleCart() {
+      this.isCartOpen = !this.isCartOpen;
     },
-    methods: {
-        toggleCart() {
-            this.isCartOpen = !this.isCartOpen;
-        },
-        addToCart(product) {
-            if (this.isInCart(product)) {
-                this.updateCart(product, product.quantity);
-            } else {
-                this.store.cartItems.push(product);
-            }
+    addToCart(product) {
+      if (this.isInCart(product)) {
+        this.updateCart(product, product.quantity);
+      } else {
+        this.store.cartItems.push(product);
+      }
 
-            localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
-        },
-        removeFromCart(index) {
-            if (this.store.cartItems[index].quantity <= 1) {
-                this.store.cartItems.splice(index, 1);
-            } else {
-                this.store.cartItems[index].quantity--;
-            }
+      localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
+    },
+    removeFromCart(index) {
+      if (this.store.cartItems[index].quantity <= 1) {
+        this.store.cartItems.splice(index, 1);
+      } else {
+        this.store.cartItems[index].quantity--;
+      }
 
       localStorage.setItem('cartItems', JSON.stringify(this.store.cartItems));
     },
@@ -61,9 +61,9 @@ export default {
 
 <template>
   <div class="nav d-flex justify-content-between align-items-center">
-    <div>
+    <router-link :to="{ name: 'home' }">
       <img src="WHEREFOOD.png" alt="">
-    </div>
+    </router-link>
     <div>
       <div class="search__container">
         <input class="search__input" type="text" placeholder="Cerca">
@@ -76,7 +76,8 @@ export default {
         <i class="fa-solid fa-cart-shopping"><span>Carrello</span></i>
         <span class="StyleCount" v-if="store.cartItems.length > 0">({{ totalCartItems }})</span>
       </button>
-      <div class="offcanvas offcanvas-end custom-offcanvas" style="width: 500px;" :class="{ 'show': isCartOpen }" id="cartOffcanvas" tabindex="-1">
+      <div class="offcanvas offcanvas-end custom-offcanvas" style="width: 500px;" :class="{ 'show': isCartOpen }"
+        id="cartOffcanvas" tabindex="-1">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title">Il tuo carrello</h5>
           <button @click="toggleCart" type="button" class="btn-close" data-bs-dismiss="offcanvas"
@@ -113,63 +114,63 @@ export default {
 }
 
 .remove-button:hover {
-    background-color: #333;
-    color: #fff;
+  background-color: #333;
+  color: #fff;
 }
 
 .nav {
-    height: 80px;
-    width: 100%;
-    padding: 0 5%;
-    background-color: #000000;
+  height: 80px;
+  width: 100%;
+  padding: 0 5%;
+  background-color: #000000;
 
 
 
 
-    i {
-        color: #575756;
-        font-size: 15px;
-        padding: 0.5rem;
+  i {
+    color: #575756;
+    font-size: 15px;
+    padding: 0.5rem;
 
 
-    }
+  }
 
-    i:hover {
-        color: #53fc5b;
-        transition: all 0.3s ease-in-out;
-    }
+  i:hover {
+    color: #53fc5b;
+    transition: all 0.3s ease-in-out;
+  }
 
 }
 
 img {
-    height: 50px;
+  height: 50px;
 }
 
 .searchBar {
-    background-color: rgba(75, 252, 113, 0);
-    border-color: #ffffff;
-    opacity: 0.5;
-    border-radius: 10px;
-    min-width: 500px;
-    height: 40px;
+  background-color: rgba(75, 252, 113, 0);
+  border-color: #ffffff;
+  opacity: 0.5;
+  border-radius: 10px;
+  min-width: 500px;
+  height: 40px;
 }
 
 .searchBar:hover {
-    opacity: 1;
+  opacity: 1;
 }
 
 .searchBtn {
-    i {
+  i {
 
-        font-size: 20px !important;
-    }
+    font-size: 20px !important;
+  }
 }
 
 ::placeholder {
-    color: #646464;
-    opacity: 0.5;
-    text-align: center;
-    font-size: 14px;
+  color: #646464;
+  opacity: 0.5;
+  text-align: center;
+  font-size: 14px;
 }
 
 .search {
@@ -177,57 +178,57 @@ img {
 
 
 
-    &__input {
-        width: 100%;
-        padding: 12px 24px;
+  &__input {
+    width: 100%;
+    padding: 12px 24px;
 
 
-        background-color: transparent;
-        transition: transform 250ms ease-in-out;
-        font-size: 14px;
-        line-height: 18px;
+    background-color: transparent;
+    transition: transform 250ms ease-in-out;
+    font-size: 14px;
+    line-height: 18px;
 
-        color: #575756;
-        background-color: transparent;
-        background-image: url(http://mihaeltomic.com/codepen/input-search/ic_search_black_24px.svg);
+    color: #575756;
+    background-color: transparent;
+    background-image: url(http://mihaeltomic.com/codepen/input-search/ic_search_black_24px.svg);
 
 
-        border-radius: 50px;
-        border: 1px solid #575756;
-        transition: all 250ms ease-in-out;
-        backface-visibility: hidden;
-        transform-style: preserve-3d;
+    border-radius: 50px;
+    border: 1px solid #575756;
+    transition: all 250ms ease-in-out;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
 
-        &::placeholder {
-            color: color(#575756 a(0.8));
-            text-transform: uppercase;
-            letter-spacing: 1.5px;
-        }
-
-        &:hover,
-        &:focus {
-
-            outline: 0;
-            border: 1px solid transparent;
-            border-bottom: 1px solid #53fc5b;
-            border-radius: 0;
-            background-position: 100% center;
-        }
+    &::placeholder {
+      color: color(#575756 a(0.8));
+      text-transform: uppercase;
+      letter-spacing: 1.5px;
     }
+
+    &:hover,
+    &:focus {
+
+      outline: 0;
+      border: 1px solid transparent;
+      border-bottom: 1px solid #53fc5b;
+      border-radius: 0;
+      background-position: 100% center;
+    }
+  }
 }
 
 .search-btn {
-    padding-left: 1rem;
+  padding-left: 1rem;
 
-    color: #646464;
-    border-radius: 50px;
-    font-weight: 700;
-    font-size: 18px;
+  color: #646464;
+  border-radius: 50px;
+  font-weight: 700;
+  font-size: 18px;
 
-    .search-btn:hover {
+  .search-btn:hover {
 
-        border-bottom: 1px solid #53fc5b !important;
-    }
+    border-bottom: 1px solid #53fc5b !important;
+  }
 }
 
 
@@ -241,21 +242,21 @@ img {
   background-color: #575756;
   color: white;
 
-    .offcanvas-title {
-        color: white;
+  .offcanvas-title {
+    color: white;
 
-    }
+  }
 
-    .btn-close {
-        color: white;
-    }
+  .btn-close {
+    color: white;
+  }
 }
 
 .round-img {
-    height: 50px;
-    width: 50px;
-    border-radius: 50%;
-    background-color: #53fc5b;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  background-color: #53fc5b;
 }
 
 .header-btn {
@@ -267,46 +268,46 @@ img {
   width: 165px;
 
 
-    span {
-        padding-left: 0.5rem
-    }
+  span {
+    padding-left: 0.5rem
+  }
 }
 
 .header-btn:hover {
-    background: none;
-    transition: all 0.5s ease-in-out;
-    border: 1px solid #646464;
+  background: none;
+  transition: all 0.5s ease-in-out;
+  border: 1px solid #646464;
 
-    span {
-        color: #646464;
-    }
+  span {
+    color: #646464;
+  }
 }
 
 @media screen and (max-width: 768px) {
-    .nav {
-        height: 50px !important;
+  .nav {
+    height: 50px !important;
 
-        img {
-            height: 20px !important;
-        }
-
-        span {
-            display: none !important;
-        }
+    img {
+      height: 20px !important;
     }
+
+    span {
+      display: none !important;
+    }
+  }
 }
 
 @media screen and (max-width : 1200px) {
-    .nav {
-        height: 60px;
+  .nav {
+    height: 60px;
 
-        img {
-            height: 50px;
-        }
-
-        span {
-            display: none;
-        }
+    img {
+      height: 50px;
     }
+
+    span {
+      display: none;
+    }
+  }
 }
 </style>

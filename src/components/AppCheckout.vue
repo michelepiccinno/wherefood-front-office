2cd9f7d578577a98d2183aab8908bff97acdc27e
andreafrancioni <andreafrancioni.work@gmail.com>
2024-03-06 16:05:11 +0100

    created submitOrder function to send customer information on db

--- src/components/AppCheckout.vue
+++ src/components/AppCheckout.vue
@@ -7,13 +7,25 @@ export default {
   data() {
     return {
       store,
+      orderData: {
+        customer_name: '',
+        customer_surname: '',
+        customer_address: '',
+        customer_number: '',
+        total_order: '100',
+        state_payment: 1,
+      },
     }
   },
-  mounted() {
-
-  },
   methods: {
-
+    async submitOrder() {
+      try {
+        const response = await axios.post(this.store.apiUrl + this.store.apiOrders, this.orderData);
+        console.log(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
   }
 }
 </script>
@@ -22,26 +34,26 @@ export default {
   <div class="container">
     <div class="row">
       <div class="col-8">
-        <form>
+        <form @submit.prevent="submitOrder" method="POST">
 
           <div class="mb-3">
             <label for="customer_name" class="form-label">Nome</label>
-            <input type="text" class="form-control" id="customer_name" aria-describedby="emailHelp">
+            <input v-model="orderData.customer_name" type="text" class="form-control" id="customer_name">
           </div>
 
           <div class="mb-3">
             <label for="customer_surname" class="form-label">Cognome</label>
-            <input type="text" class="form-control" id="customer_surname">
+            <input v-model="orderData.customer_surname" type="text" class="form-control" id="customer_surname">
           </div>
 
           <div class="mb-3">
             <label for="customer_address" class="form-label">Indirizzo del cliente</label>
-            <input type="text" class="form-control" id="customer_address">
+            <input v-model="orderData.customer_address" type="text" class="form-control" id="customer_address">
           </div>
 
           <div class="mb-3">
             <label for="customer_number" class="form-label">Telefono del cliente</label>
-            <input type="text" class="form-control" id="customer_number">
+            <input v-model="orderData.customer_number" type="text" class="form-control" id="customer_number">
           </div>
 
           <div class="mb-3">
@@ -49,14 +61,12 @@ export default {
             <input type="text" class="form-control" id="customer_number">
           </div>
 
-          <!-- Button trigger modal -->
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <button type="submit" class="btn btn-primary">
             INVIA
           </button>
 
-          <!-- Modal -->
-          <div class="modal fade text-dark" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+          <div class="modal fade text-dark" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
@@ -65,7 +75,7 @@ export default {
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">chiudi</button>
-                <!--   <button type="button" class="btn btn-primary">Invia</button> -->
+                  <!--   <button type="button" class="btn btn-primary">Invia</button> -->
                 </div>
               </div>
             </div>
--- src/store.js
+++ src/store.js
@@ -4,6 +4,7 @@ export const store = reactive({
     apiUrl: "http://127.0.0.1:8000/api/",
     apiRestaurants: "restaurants/",
     apiCategories: "categories/",
+    apiOrders: "orders/",
     restaurantsArray: [],
     filteredRestaurants: [],
     categoriesArray: [],
